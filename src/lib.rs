a77f41cf732b82fc6051ab78f3e68a68ce3d8e7e
Björn Guth <b.guth@netcon-consulting.com>
2021-07-09 14:27:49 +0200

    Create a first working version to report to Kimai

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,118 @@
-use timewarrior_report::get_data;
+use futures::future::try_join_all;
+use kimai::{load_config, log_timesheet_record, Config};
+use std::fmt;
+use std::process::Command;
+use timewarrior_report::{get_data, Session};
 
-pub fn run() {
-    dbg!(get_data().unwrap());
+#[derive(Debug)]
+pub enum ReportError {
+    Kimai(String),
+    Timewarrior(String),
+    ParseInt(String),
+    IO(String),
+    Other(String),
+}
+
+impl std::error::Error for ReportError {}
+
+impl fmt::Display for ReportError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Kimai(e) => write!(f, "Kimai Error: {}", e),
+            Self::Timewarrior(e) => write!(f, "Timewarrior Error: {}", e),
+            Self::ParseInt(e) => write!(f, "Parse Int Error: {}", e),
+            Self::IO(e) => write!(f, "IO Error: {}", e),
+            Self::Other(e) => write!(f, "Other Error: {}", e),
+        }
+    }
+}
+
+impl From<timewarrior_report::ReportError> for ReportError {
+    fn from(error: timewarrior_report::ReportError) -> Self {
+        Self::Timewarrior(error.to_string())
+    }
+}
+
+impl From<kimai::KimaiError> for ReportError {
+    fn from(error: kimai::KimaiError) -> Self {
+        Self::Kimai(error.to_string())
+    }
+}
+
+impl From<std::num::ParseIntError> for ReportError {
+    fn from(error: std::num::ParseIntError) -> Self {
+        Self::ParseInt(error.to_string())
+    }
+}
+
+impl From<std::io::Error> for ReportError {
+    fn from(error: std::io::Error) -> Self {
+        Self::IO(error.to_string())
+    }
+}
+
+fn parse_kimai_id(input: &str, identifier: &str) -> Result<Option<usize>, ReportError> {
+    if input.starts_with(identifier) {
+        Ok(Some(input.split(':').collect::<Vec<_>>()[1].parse()?))
+    } else {
+        Ok(None)
+    }
+}
+
+async fn log_session(config: &Config, session: Session) -> Result<(), ReportError> {
+    let mut kimai_project: Option<usize> = None;
+    let mut kimai_activity: Option<usize> = None;
+    let mut kimai_id: Option<usize> = None;
+    let mut tags = Vec::new();
+
+    for tag in &session.tags {
+        if let Some(i) = parse_kimai_id(tag, "kimai_project")? {
+            kimai_project = Some(i);
+        } else if let Some(i) = parse_kimai_id(tag, "kimai_activity")? {
+            kimai_activity = Some(i);
+        } else if let Some(i) = parse_kimai_id(tag, "kimai_id")? {
+            kimai_id = Some(i);
+        } else {
+            tags.push(tag.clone());
+        }
+    }
+
+    if let Some(id) = kimai_id {
+        println!("@{}: already got logged with ID {}", session.id, id);
+    } else if let (Some(project_id), Some(activity_id)) = (kimai_project, kimai_activity) {
+        let record = log_timesheet_record(
+            &config,
+            0,
+            project_id,
+            activity_id,
+            session.start,
+            session.end,
+            session.annotation,
+            Some(tags),
+        )
+        .await?;
+        let _cmd_result = Command::new("timew")
+            .arg("tag")
+            .arg(format!("@{}", session.id))
+            .arg(format!("kimai_id:{}", record.id))
+            .output()?;
+        println!("@{}: logged to Kimai", session.id);
+    } else {
+        println!("@{}: required IDs not found!", session.id);
+    }
+    Ok(())
+}
+
+#[tokio::main]
+pub async fn run(config_path: Option<String>) -> Result<(), ReportError> {
+    let config = load_config(config_path)?;
+    let timewarrior_data = get_data()?;
+
+    let mut future_vec = Vec::new();
+    for session in timewarrior_data.sessions {
+        future_vec.push(log_session(&config, session))
+    }
+    let _results = try_join_all(future_vec).await?;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
+
 fn main() {
-    kimai_report::run();
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .about(crate_description!())
+        .author(crate_authors!())
+        .arg(
+            Arg::with_name("config_path")
+                .long("config_path")
+                .short("c")
+                .takes_value(true),
+        )
+        .get_matches();
+    kimai_report::run(matches.value_of("config_path").map(|p| p.into())).unwrap();
 }
