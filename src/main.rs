6e83c5114a031ee0364b180b956be704e006c94e
Björn Guth <b.guth@netcon-consulting.com>
2021-09-26 14:52:02 +0200

    Start working on a subcommand for comparisons

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg, SubCommand};
+
+mod wrapper;
 
 fn main() {
     let matches = App::new(crate_name!())
@@ -11,6 +13,27 @@ fn main() {
                 .short("c")
                 .takes_value(true),
         )
+        .subcommand(
+            SubCommand::with_name("report_wrapper").subcommand(
+                SubCommand::with_name("install").arg(
+                    Arg::with_name("timew_path")
+                        .long("timew_path")
+                        .short("p")
+                        .takes_value(true)
+                        .help("Path to the Timewarrior directory"),
+                ),
+            ),
+        )
         .get_matches();
-    kimai_report::run(matches.value_of("config_path").map(|p| p.into())).unwrap();
+
+    if let Some(matches) = matches.subcommand_matches("report_wrapper") {
+        dbg!(matches);
+        if let Some(matches) = matches.subcommand_matches("install") {
+            wrapper::install_report(matches.value_of("timew_path").map(|p| p.into())).unwrap();
+        } else {
+            wrapper::test_wrapper();
+        }
+    } else {
+        kimai_report::run(matches.value_of("config_path").map(|p| p.into())).unwrap();
+    }
 }
